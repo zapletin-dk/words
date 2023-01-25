f404bda08aa2c5c6d72ef4ee6dd831f1eaedc3b7
Zapletin Daniil <Zaplot1994@gmail.com>
2023-01-25 19:36:15 +0300

    Task done with all tests passed

--- src/main/java/com/epam/rd/autotasks/words/StringUtil.java
+++ src/main/java/com/epam/rd/autotasks/words/StringUtil.java
@@ -1,28 +1,86 @@
 package com.epam.rd.autotasks.words;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 
 public class StringUtil {
     public static int countEqualIgnoreCaseAndSpaces(String[] words, String sample) {
-        throw new UnsupportedOperationException();
+        int counter = 0;
+        if (words != null && sample != null) {
+            for (String word : words) {
+                if (sample.trim().equalsIgnoreCase(word.trim())) {
+                    counter++;
+                }
+            }
+            return counter;
+        } else { return 0; }
     }
 
     public static String[] splitWords(String text) {
-        throw new UnsupportedOperationException();
+        if (text != null && !text.isBlank()) {
+            String string = Pattern.compile("[,.;: ?!]+").matcher(text).replaceAll(" ").strip();
+            return string.isBlank() ? null : string.split(" ");
+        } else {return null; }
     }
 
-    public static String convertPath(String path, boolean toWin) {
-        throw new UnsupportedOperationException();
+    public static String convertPath(String path, boolean toWin){
+        if (path == null || path.isEmpty() || !(isWindowsMatches(path) || isUnixMatches(path))) {
+            return null;
+        }
+        if ((isWindowsMatches(path) && toWin) || (isUnixMatches(path) && !toWin)) {
+            return path;
+        }
+        String resultPath;
+        if (toWin) {
+            resultPath = convertPathToWindows(path);
+        } else {
+            resultPath = convertPathToUnix(path);
+        }
+        return resultPath;
+    }
+    private static boolean isUnixMatches(String path) {
+        return path.matches("([/~])?([\\w.\\-\\s/]*)");
+    }
+
+    private static boolean isWindowsMatches(String path) {
+        return path.matches("(C:\\\\)?([\\w.\\-\\s\\\\]*)");
+    }
+
+    private static String convertPathToUnix(String path) {
+        if (path.startsWith("C:\\User")) {
+            path = path.replace("C:\\User", "~");
+        } else if (path.startsWith("C:\\")) {
+            path = path.replace("C:\\", "/");
+        }
+        return path.replace("\\", "/");
+    }
+
+    private static String convertPathToWindows(String path) {
+        if (path.startsWith("~")) {
+            path = path.replace("~", "C:\\User");
+        } else if (path.startsWith("/")) {
+            path = path.replaceFirst("/", "C:\\\\");
+        }
+        return path.replace("/", "\\");
     }
 
     public static String joinWords(String[] words) {
-        throw new UnsupportedOperationException();
+        String str;
+        if (words != null){
+            str = Arrays.stream(words)
+                    .filter(role -> !role.isBlank())
+                    .collect(Collectors.joining(", "));
+            System.out.println(str);
+            return str.isBlank() ? null : "[" + str + "]";
+        } else { return null; }
     }
 
     public static void main(String[] args) {
         System.out.println("Test 1: countEqualIgnoreCaseAndSpaces");
         String[] words = new String[]{" WordS    \t", "words", "w0rds", "WOR  DS", };
-        String sample = "words   ";
+        String sample = "words";
         int countResult = countEqualIgnoreCaseAndSpaces(words, sample);
         System.out.println("Result: " + countResult);
         int expectedCount = 2;
@@ -43,10 +101,11 @@ public class StringUtil {
         System.out.println("Must be: " + expectedWinPath);
 
         System.out.println("Test 4: joinWords");
-        String[] toJoin = new String[]{"go", "with", "the", "", "FLOW"};
+        String[] toJoin = new String[]{"go", "with", "the", "", "FLOW" , " "};
         String joinResult = joinWords(toJoin);
         System.out.println("Result: " + joinResult);
         String expectedJoin = "[go, with, the, FLOW]";
         System.out.println("Must be: " + expectedJoin);
+
     }
 }
